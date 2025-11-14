e7a65b145e8b950ce531251b05a8fef861ad3549
Radovan Tkac <radovan.tkac@gjensidige.dk>
2025-11-14 23:11:16 +0100

    feat: netlify setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
+import netlify from "@netlify/vite-plugin-tanstack-start";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import { intlayer, intlayerProxy } from "vite-intlayer";
-
-
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
   plugins: [
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
     tailwindcss(),
     tanstackStart(),
+    netlify(),
     viteReact(),
     intlayer(),
     intlayerProxy(),
-
   ],
-})
+});
 
-export default config
+export default config;
