b891d96868b76a02c26ddf0904a2eb97e984da60
Radovan Tkac <radovan.tkac@gjensidige.dk>
2025-11-28 16:26:33 +0100

    feat: enabled analytics on for production

--- src/components/Disclaimer/analytics.ts
+++ src/components/Disclaimer/analytics.ts
@@ -1,5 +1,8 @@
 import ReactGA from "react-ga4";
 
 export function initializeAnalytics() {
-  ReactGA.initialize("G-PDDJLQ8NLZ");
+  console.log(process.env.NODE_ENV);
+  if (process.env.NODE_ENV === "production") {
+    ReactGA.initialize("G-PDDJLQ8NLZ");
+  }
 }
