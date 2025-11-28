afa253d9a4aba0deae9975c550eaa9e8f3d7c47f
Radovan Tkac <radovan.tkac@gjensidige.dk>
2025-11-28 17:15:19 +0100

    fix: updated disclaimer styles

--- src/components/Disclaimer/Disclaimer.tsx
+++ src/components/Disclaimer/Disclaimer.tsx
@@ -2,33 +2,26 @@ import CookieConsent from "react-cookie-consent";
 
 import { initializeAnalytics } from "./analytics";
 
+import { m } from "@/paraglide/messages";
+
 const Disclaimer = () => (
   <CookieConsent
+    disableStyles
     enableDeclineButton
     expires={150}
     location="bottom"
     buttonText="Accept"
     declineButtonText="Decline"
-    style={{
-      background: "#2b2b2b",
-      color: "#ffffff",
-    }}
-    buttonStyle={{
-      background: "#4caf50",
-      color: "#ffffff",
-      fontSize: "14px",
-    }}
-    declineButtonStyle={{
-      background: "#f44336",
-      color: "#ffffff",
-      fontSize: "14px",
-    }}
+    buttonClasses="btn-primary"
+    declineButtonClasses="btn-secondary mr-4"
+    contentClasses="mb-4 max-w-3xl mx-auto text-sm md:text-base"
+    containerClasses="bg-primary-light fixed bottom-0 z-50 w-full p-4 text-center md:p-6"
+    buttonStyle={{}}
     onAccept={() => {
       initializeAnalytics();
     }}
   >
-    We use analytics cookies to understand how visitors interact with our website and to help us
-    improve it. You can choose to accept or decline these cookies.
+    {m.disclaimer_text()}
   </CookieConsent>
 );
 
