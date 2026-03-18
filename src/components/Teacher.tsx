b1230d635f95e6894e2178d6fd4837267fb9c1fa
Radovan Tkac <tkac.radovan@gmail.com>
2026-03-18 22:17:06 +0100

    fix: replaced images with placehoders

--- src/components/Teacher.tsx
+++ src/components/Teacher.tsx
@@ -4,7 +4,11 @@ const Teacher = () => {
   return (
     <div className="space-y-7 rounded-md border border-primary-light bg-white px-5 py-8 text-center md:flex md:space-y-0 md:p-10 md:text-left dark:border-tertiary-light dark:bg-tertiary">
       <div className="mx-auto h-35 w-35 overflow-hidden rounded-full md:mr-8 md:ml-0">
-        <img src="/home/1.jpg" alt="2" className="h-full w-full object-cover" />
+        <img
+          src="https://placehold.co/140x140/4a413a/4a413a"
+          alt="2"
+          className="h-full w-full object-cover"
+        />
       </div>
       <div className="md:max-w-120">
         <h2 className="font-bold text-primary text-sm">{m.teacher_title()}</h2>
