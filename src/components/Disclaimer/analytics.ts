3ae26e30fe595c5ca8275a88419ab385606cf2d6
Radovan Tkac <tkac.radovan@gmail.com>
2026-04-10 23:41:27 +0200

    Revert "Merge pull request #69 from rtkac/develop"
    This reverts commit d09d34eb3bc8628a8077d01efbfc3ddcc6ba10e4, reversing
    changes made to 1c7f8e528a5ec44b716915b172f85b02618f7961.

--- src/components/Disclaimer/analytics.ts
+++ src/components/Disclaimer/analytics.ts
@@ -2,9 +2,6 @@ import ReactGA from "react-ga4";
 
 export const initializeAnalytics = () => {
   if (process.env.NODE_ENV === "production") {
-    const apiKey = import.meta.env.VITE_GOOGLE_ANALYTICS_API_KEY;
-    if (apiKey) {
-      ReactGA.initialize(apiKey);
-    }
+    ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_API_KEY);
   }
 };
